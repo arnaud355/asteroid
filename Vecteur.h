001a6605f66d041c8529681d4f5c427c0e5a3c69
arnaud355 <33963757+arnaud355@users.noreply.github.com>
2019-05-14 21:49:17 +0200

    Update Vecteur.h

--- Vecteur.h
+++ Vecteur.h
@@ -7,7 +7,7 @@ struct  Vecteur
 {
 	void operator+=(Vecteur const& autre);
 	void operator-=(Vecteur const& autre);
-	Vecteur operator*=(float coefficient) const;
+	Vecteur operator*(float coefficient) const;
 	static Vecteur CreerDepuisAngle(float taille,float angleEnDegre);
 	float x{ 0.f };
 	float y{ 0.f };
