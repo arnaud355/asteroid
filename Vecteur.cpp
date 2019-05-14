087d4d449497c80cd7448738b6330d8b40133452
arnaud355 <33963757+arnaud355@users.noreply.github.com>
2019-05-14 20:53:58 +0200

    Create Vecteur.cpp

---
+++ Vecteur.cpp
@@ -0,0 +1,22 @@
+#include "Vecteur.h"
+#include <cmath>
+
+void Vecteur::operator+=(Vecteur const& autre)
+{
+	x += autre.x;
+	y += autre.y;
+}
+
+void Vecteur::operator-=(Vecteur const& autre)
+{
+	x -= autre.x;
+	y -= autre.y;
+}
+
+Vecteur Vecteur::operator*=(float coefficient) const
+{
+	return { x * coefficient,y * coefficient };
+}
+Vecteur Vecteur::CreerDepuisAngle(float taille, float angleEnDegre) {
+	return { taille * cos(angleEnDegre / 180.f * M_PI),taille * sin(angleEnDegre / 180.f * M_PI) };
+}
